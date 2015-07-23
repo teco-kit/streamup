c08cb9cc90925018fbe135c084ade7f667069e87
Michael Jacoby <jacoby@teco.edu>
2015-07-23 12:57:26 +0200

    cleaning up project structure

--- streamup-bundle.js
+++
@@ -1,8 +0,0 @@
-Error: Cannot find module '_http_common' from '/usr/src/app/node_modules/restify/node_modules/spdy/lib/spdy'
-    at /usr/local/lib/node_modules/browserify/node_modules/resolve/lib/async.js:46:17
-    at process (/usr/local/lib/node_modules/browserify/node_modules/resolve/lib/async.js:173:43)
-    at ondir (/usr/local/lib/node_modules/browserify/node_modules/resolve/lib/async.js:188:17)
-    at load (/usr/local/lib/node_modules/browserify/node_modules/resolve/lib/async.js:69:43)
-    at onex (/usr/local/lib/node_modules/browserify/node_modules/resolve/lib/async.js:92:31)
-    at /usr/local/lib/node_modules/browserify/node_modules/resolve/lib/async.js:22:47
-    at Object.oncomplete (fs.js:108:15)
